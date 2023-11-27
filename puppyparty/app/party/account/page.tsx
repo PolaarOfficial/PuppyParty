5bb5c5aa48f0139ba7b8d04c5ab91fae2ac7627a
polaar <hughrulev@gmail.com>
2023-11-26 22:48:16 -0500

    continued estup

--- puppyparty/app/party/account/page.tsx
+++ puppyparty/app/party/account/page.tsx
@@ -1,4 +1,4 @@
-import CurrentAccount from "@/app/ui/currentaccount";
+import CurrentAccount from '@/app/ui/currentaccount';
 
 export default function Page(){
     return (
--- puppyparty/app/ui/currentaccount.tsx
+++ puppyparty/app/ui/currentaccount.tsx
@@ -1,15 +1,15 @@
-import { getCurrentAccountDetails } from "@/app/lib/data";
-export default async function CurrentAccount(){
-    const currentAccount = await getCurrentAccountDetails();
+// import { getCurrentAccountDetails } from "@/app/lib/data";
+export default function CurrentAccount(){
+    // const currentAccount = await getCurrentAccountDetails();
     return (
         <div>
-            {currentAccount.map(()=>{
+            {/* {currentAccount.map(()=>{
                 return(
                     <div>
 
                     </div>
                 );
-                })}
+                })} */}
         </div>
     );
 
